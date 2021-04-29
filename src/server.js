3b346f264a81245809749a9a23ee64fd5d3af9b7
cacingtana <51318143+cacingtana@users.noreply.github.com>
2021-04-30 00:10:51 +0800

    fix bug host singapure

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const routes = require('./routes');
 const init = async () => {
     const server = Hapi.server({
         port: 5000,
-        host: process.env.NODE_ENV !== 'production' ? 'localhost' : '172.31.9.183',
+        host: process.env.NODE_ENV !== 'production' ? 'localhost' : '172.31.39.104',
         routes: {
             cors: {
               origin: ['*'],
